3fa8da319ff42dc50021afa281ebda27b848cd2c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-16 17:05:22 -0500

    Handle progress bar progress on mouse click and drag

--- scripts.js
+++ scripts.js
@@ -30,10 +30,22 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e)
+}
+
+function handleProgress () {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
@@ -42,5 +54,11 @@ ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousedown', e => isDown = true));
 ranges.forEach(range => range.addEventListener('mouseup', () => isDown = false));
 ranges.forEach(range => range.addEventListener('mouseout', () => isDown = false));
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => isDown && scrub(e));
+progress.addEventListener('mousedown', () => isDown = true);
+progress.addEventListener('mouseup', () => isDown = false);
+
 
 
